be5efc65c501f98f1ff2a8c9440fde1f83088111
Elleyse Na <nae122@newschool.edu>
2023-12-06 12:02:47 -0500

    I added files and edited index

---
+++ script.js
@@ -0,0 +1,29 @@
+// const baseURL = "https://web-cafe.onrender.com/"
+
+// const color = "Green";
+
+// const getCars = async function(){
+//     try{
+//         const carIDs = await fetch(`${baseURL}?color=${color}`)
+//         const carIDsJSON = await carIDs.json();
+
+//         console.log(carIDsJSON)
+
+//         Object.keys(carIDsJSON).forEach(async (key,value) => {
+//             try{
+//                 const carInfo = await fetch()
+
+//             } catch(error){
+//                 console.log(error)
+//             }
+//         })
+
+//         // const carh1 = document/getElementById("carIDs");
+//         // carh1.innerHTML = carIDsJSON; 
+
+//     }catch(error) {
+//         console.log(error)
+//     }
+// }
+
+// getCars();
